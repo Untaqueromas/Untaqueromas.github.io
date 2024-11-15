85525e5ed3ea08cd08ed9f29beeedb34db31a275
unknown <benjy0427pa@gmail.com>
2024-11-15 11:07:52 -0600

    modificacion

--- self.js
+++ self.js
@@ -5,7 +5,16 @@ self.addEventListener('install', (event) => {
       })
     );
   });
-  
+// Archivos que se deben almacenar en caché
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/assets/image/icon-144.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
+];
+
   self.addEventListener('fetch', (event) => {
     event.respondWith(
       caches.match(event.request)
