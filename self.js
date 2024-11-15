add1875d23c6916e45e3b850aa9702a36949c07e
unknown <benjy0427pa@gmail.com>
2024-11-15 11:35:19 -0600

    remplazos

--- self.js
+++ self.js
@@ -1,26 +1,28 @@
+// Archivos que se deben almacenar en caché
+const FILES_TO_CACHE = [
+  // '/',
+  './index.html',
+  '/manifest.json',
+  './fondos-de-pantalla-del-espacio-para-pc-4-1200x675.jpeg',
+  './bootstrap.min.css',
+  './bootstrap.bundle.min.js',
+];
+
 self.addEventListener('install', (event) => {
     event.waitUntil(
       caches.open('static-resources').then((cache) => {
-        return cache.addAll(['/index.html', '/app.js', '/styles.css']);
+        return cache.addAll(FILES_TO_CACHE);
       })
     );
   });
-// Archivos que se deben almacenar en caché
-const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/assets/image/icon-144.png',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
-];
+
 
   self.addEventListener('fetch', (event) => {
     event.respondWith(
       caches.match(event.request)
         .then((response) => {
-          return response   
-   || fetch(event.request);
+          return response
+      // fetch(event.request);
         })
     );
   });
