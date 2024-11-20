50301fc451c495a8db174ab237b19a6ded480b93
unknown <benjy0427pa@gmail.com>
2024-11-20 08:55:27 -0600

    cambios

--- self.js
+++ self.js
@@ -1,4 +1,4 @@
-// Archivos que se deben almacenar en caché
+// 
 const FILES_TO_CACHE = [
   // '/',
   './index.html',
@@ -16,13 +16,42 @@ self.addEventListener('install', (event) => {
     );
   });
 
+  self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (!cacheWhitelist.includes(cacheName)) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
 
   self.addEventListener('fetch', (event) => {
     event.respondWith(
-      caches.match(event.request)
-        .then((response) => {
-          return response
-      // fetch(event.request);
-        })
+      caches.match(event.request).then((cachedResponse) => {
+        // Si el archivo está en caché, devuelve la respuesta de caché
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+  
+        // Si el archivo no está en caché, hacer una solicitud a la red
+        return fetch(event.request).then((networkResponse) => {
+          // Si la respuesta de la red es válida, guarda la respuesta en caché
+          if (networkResponse && networkResponse.status === 200) {
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, networkResponse.clone());
+            });
+          }
+          return networkResponse;
+        }).catch(() => {
+          // Si no hay red, responde con una página de "offline"
+          return caches.match('/offline.html');
+        });
+      })
     );
   });
