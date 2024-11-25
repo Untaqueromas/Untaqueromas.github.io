54a853d952de8654f7a2667c94d11875f961725e
unknown <benjy0427pa@gmail.com>
2024-11-25 11:59:15 -0600

    Test deploy

--- self.js
+++ self.js
@@ -1,7 +1,5 @@
-// aaa
 let CACHE_VERSION = 'app-v0.1';
-let FILES_TO_CACHE =[
-  // '/',
+let FILES_TO_CACHE = [
   './index.html',
   '/manifest.json',
   './fondos-de-pantalla-del-espacio-para-pc-4-1200x675.jpeg',
@@ -12,35 +10,36 @@ let FILES_TO_CACHE =[
 self.addEventListener('install', function (event) {
   self.skipWaiting();
   event.waitUntil(
-      caches.open(CACHE_VERSION)
-          .then(function (cache) {
-              console.log('Opened cache');
-              return cache.addAll(FILES_TO_CACHE);
-          })
-  )
-})
+    caches.open(CACHE_VERSION)
+      .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(FILES_TO_CACHE);
+      })
+  );
+});
 
 self.addEventListener('fetch', function (event) {
-  let online = navigator.onLine
-  if (!online) {
-      event.respondWith(
-          caches.match(event.request).then(function (res) {
-              if (res) {
-                  return res;
-              }
-          })
-      )
-  }
-})
+  event.respondWith(
+    caches.match(event.request).then(function (res) {
+      if (res) {
+        // Return the cached response if available
+        return res;
+      }
+      // Otherwise, fetch from the network
+      return fetch(event.request);
+    })
+  );
+});
 
-self.addEventListener('activate', function(event){
+self.addEventListener('activate', function (event) {
   event.waitUntil(
-      caches.keys().then(function(keys){
-          return prompt.all(keys.map(function(keys, i){
-              if(keys !== CACHE_VERSION){
-                  return caches.delete(keys[i]);
-              }
-          }))
-      })
-  )
-})
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (key) {
+        if (key !== CACHE_VERSION) {
+          console.log('Deleting old cache:', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
